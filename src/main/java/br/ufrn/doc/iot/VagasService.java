3c83bfbfc0bc026407374e609e6d302b84fe0786
Mário Melo <mariovmelo@gmail.com>
2017-05-18 13:29:49 -0300

    aJUSTES no app para funcionar corretamente

--- src/main/java/br/ufrn/doc/iot/VagasService.java
+++ src/main/java/br/ufrn/doc/iot/VagasService.java
@@ -3,13 +3,20 @@ package br.ufrn.doc.iot;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import br.ufrn.doc.iot.dominio.Estacionamento;
 import br.ufrn.doc.iot.dominio.Vaga;
@@ -29,5 +36,25 @@ public class VagasService {
 		
 		return estacionamento.getVagasEstacionamento();
 
+	}
+	
+	@POST
+	@Path("/associarDevice")
+	@Consumes("application/json")
+	public Response associarClienteTagRFID(@FormParam("token")String tokenFCM,@FormParam("tag") String tagRFID){
+		
+		if(tokenFCM == null || tagRFID == null){
+			return Response.status(Status.NOT_ACCEPTABLE).build();
+		}
+		
+		Map<String, String> mapaTags = (Map<String, String>) context.getAttribute("dadosUsuarios");
+		
+		if(mapaTags == null){
+			mapaTags = new HashMap<String,String>();
+		}
+		
+		mapaTags.put(tagRFID, tokenFCM);
+		
+		return Response.ok().build();
 	}
 }
